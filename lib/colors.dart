7da8a67eacc14df00169bc3b3cd1c6a80036c691
Martin Kaněra <marti.kanera@gmail.com>
2021-02-11 20:56:49 +0100

    feat(colours): added default colours

---
+++ lib/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class PrimaryColor {
+  PrimaryColor._();
+
+  static Color _black = Color(0xff1C1D21);
+  static Color _pink = Color(0xffDE4D86);
+
+  static Color get black {
+    return _black;
+  }
+
+  static Color get pink {
+    return _pink;
+  }
+}
